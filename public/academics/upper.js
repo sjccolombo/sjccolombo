1f4e9756fc2ca20ed765cc79481632e3a375833f
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-04-14 13:35:58 +0530

    Finished Academics I (main)

---
+++ public/academics/upper.js
@@ -0,0 +1,25 @@
+// Wait for DOM to finish loading before running script
+document.addEventListener('DOMContentLoaded', () => {
+    // Use fetch to get the JSON data
+    fetch('https://raw.githubusercontent.com/sjccolombo/sjc-storage/main/JSON/main/upper.json')
+        .then(response => response.json())
+        .then(data => {
+            // Get the container element
+            let container = document.getElementById('education-content');
+            let htmlData = '';
+            // Loop through the data array
+            for (let i = 0; i < data.length; i++) {
+                htmlData += '<div class="education-contentCard">';
+                htmlData += '<a href="' + data[i].link + '">';
+                htmlData += '<h2>' + data[i].grade + '</h2>';
+                htmlData += '</a>';
+                htmlData += '<img src="' + data[i].image + '" alt="' + data[i].imageAlt + '">';
+                htmlData += '</div>';
+            }
+            // Insert the HTML into the container
+            container.innerHTML = htmlData;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
