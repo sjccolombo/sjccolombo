439769bd868bd32392a0d6076edfcb466fffc853
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-04-15 13:18:12 +0530

    Fixes for menu

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const toTopBtn = document.querySelector('#to-top-btn');
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('show');
+    // Prevent Scrolling of the Main When Menu Opened
+    document.body.classList.toggle('menu-open');
+    // Toggle visibility of to-top button
+    toTopBtn.style.visibility = (navMenu.classList.contains('show')) ? 'hidden' : 'visible';
 });
