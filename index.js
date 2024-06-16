602aaf0ae7ac98ad1f83e2a637435ca57fcb4ee0
sandunwira <79461263+sandunwira@users.noreply.github.com>
2024-06-16 23:51:03 +0530

    Updated Scripts and Dependencies | Updated Homepage Links

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
+const port = 8080;
+var favicon = require('serve-favicon');
 
-app.use(express.static(__dirname + "/public"), (_, res, next) => {
-	res.status(404)
-	res.sendFile(__dirname + "/404.html")
+
+// Static Files Start
+app.use(express.static(path.join(__dirname, 'public')));
+// Static Files End
+
+
+// Favicon Start
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'favicon.png')));
+// Favicon End
+
+
+// URL Routes Start
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+// URL Routes End
 
-app.listen(8080);
+
+// Error Handling Start
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+// Error Handling End
+
+
+// Port Configuration Start
+app.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
+});
+// Port Configuration End
